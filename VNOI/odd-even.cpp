89dcd6da89b952366c4738b48c84559edf869d87
juniokun <vinhng74@gmail.com>
2022-09-06 21:54:36 +0700

    /

---
+++ VNOI/odd-even.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void solve(){
+	int n;
+	cin>>n;
+
+	if(n % 2 == 0){
+		cout << "YES" << "\n";
+
+	}
+	else {
+		cout << "NO" <<"\n";
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	cout.tie(NULL);
+	solve();
+	return 0;
+}
