0c157e0d3c6cce3fa4db9d4688e16c60450b19be
juniokun <vinhng74@gmail.com>
2022-09-13 08:45:22 +0700

    /

---
+++ VNOI/songuyento.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <math.h>
+
+using namespace std;
+
+void solve(){
+    long a, b, n;
+    cin>>a>>b;
+
+    for(int i = a; i <= b; i++){
+        if(i == 1)
+            continue;
+        n = 0;
+        for(int j = 2; j <= sqrt(i); j++ ){
+            if(i % j == 0)
+            {
+                n++;
+                break;
+            }
+        }
+        if(n == 0){ 
+            cout<<i<<"\n";
+        }   
+        
+    }
+    
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
+    return 0;
+}
--- codeforces/4/A.cpp
+++ codeforces/4/A.cpp
@@ -11,7 +11,7 @@ void solve(){
 		cout<<"NO"<<"\n";
 	}
 }
- 
+
  
  
 int main(){
