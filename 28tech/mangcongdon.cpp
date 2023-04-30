6b4efd6fb46b3d7cbd54231fccba9a893241fab0
vinhhnq <vinhng74@gmail.com>
2023-05-01 00:11:38 +0700

    commit 1/5/2023 00:11

---
+++ 28tech/mangcongdon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n; cin>>n;
+	int a[n+1];	
+	for (int i = 1; i <= n; i++) cin>>a[i];
+	int prefix[n + 1] = {0};
+	for (int i = 1; i <= n; ++i){
+		prefix[i] = prefix[i-1] + a[i];
+	}
+	int q; cin>>q;
+	while(q--) {
+		int l,r; cin>>l>>r;
+		cout<<prefix[r] + prefix[l-1]<<endl;
+	}
+	return 0; 
+}
--- test.cpp
+++ test.cpp
@@ -1,12 +1,24 @@
-#include <bits/stdc++.h> 
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	
-	// code here;
-	auto v =2;
-	cout<<v<<endl;
+	int n,m; cin>>n>>m;
+	int a[n][m];
+
+	for (int i = 0; i < n; ++i)
+	{
+			for (int j = 0; j < m; ++j)
+		{
+			cin>>a[i][j];
+		}
+	}
+	for (int i = 0; i < n; ++i)
+	{
+			for (int j = 0; j < m; ++j)
+		{
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
 	return 0;
 }
