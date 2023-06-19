2072fe58b1537f302de30fb93c890b5010448009
vinhhnq <vinhng74@gmail.com>
2023-06-20 00:23:10 +0700

    Update gdturn.cpp

---
+++ codechef/basic_programming/gdturn.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int x,y; cin>>x>>y;
+    if(x + y > 6){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+int main() {
+	int t; cin>>t;
+	while(t--){
+	    solve();
+	}
+	return 0;
+}
